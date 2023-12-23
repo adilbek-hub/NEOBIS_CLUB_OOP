301c05b3c2c8e171f66b38890970bbf730c02a4c
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-12-23 11:39:52 +0600

    lesson_1

---
+++ lesson_1_introduction/bin/lesson_1_introduction.dart
@@ -0,0 +1,4 @@
+void main(List<String> arguments) {
+  Map<String, dynamic> myDict = {"ali": 0777705197, "age": 29};
+  print(myDict["ali"]);
+}
---
+++ lesson_1_introduction/test/lesson_1_introduction_test.dart
@@ -0,0 +1,8 @@
+import 'package:lesson_1_introduction/lesson_1_introduction.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
